e62f5c89a1198dd0616955988b29d4e5029fc693
naoyakumagai <naoya.3022@gmail.com>
2017-04-14 07:56:44 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'messages#index'
+
+  #以下にCRUDなどの記述が含まれている。
+  resources :messages
 end
